31b73f7108ebf475d8b816dde8ab69961b960ee0
wangqiang <wangqiang.wei@cootek.cn>
2017-08-10 15:07:26 +0800

    [fix] gradle

--- app/src/main/java/juhe/jiangdajiuye/core/MyApplication.java
+++ app/src/main/java/juhe/jiangdajiuye/core/MyApplication.java
@@ -37,7 +37,7 @@ public class MyApplication extends Application {
         super.onCreate();
         application = this;
         context = this;
-        JPushInterface.setDebugMode(true); 	// 设置开启日志,发布时请关闭日志
+        JPushInterface.setDebugMode(false); 	// 设置开启日志,发布时请关闭日志
         JPushInterface.init(this);     		// 初始化 JPush
         Bmob.initialize(this, Bmob_AppId);
 
