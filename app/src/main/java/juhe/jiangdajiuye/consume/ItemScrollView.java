da22693aef6f9ba90b54961f6c6d5c033cb30908
wangqiang <1198039102wei@gmail.com>
2017-12-31 13:53:32 +0800

    [feature] add upgrade

---
+++ app/src/main/java/juhe/jiangdajiuye/consume/ItemScrollView.java
@@ -0,0 +1,11 @@
+package juhe.jiangdajiuye.consume;
+
+/**
+ * class description here
+ *
+ * @author wangqiang
+ * @since 2017-12-31
+ */
+
+public class ItemScrollView {
+}
---
+++ app/src/main/java/juhe/jiangdajiuye/util/StringUtils.java
@@ -0,0 +1,11 @@
+package juhe.jiangdajiuye.util;
+
+/**
+ * class description here
+ *
+ * @author wangqiang
+ * @since 2017-12-31
+ */
+
+public class StringUtils {
+}
