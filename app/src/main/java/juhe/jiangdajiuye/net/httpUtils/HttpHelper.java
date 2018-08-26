25bc481af0438a057f84111e6e68d0531d755f4a
weiwangqiang <l>
2018-08-26 08:59:08 +0800

    [fix] fix bug

--- app/src/main/java/juhe/jiangdajiuye/net/httpUtils/HttpHelper.java
+++ app/src/main/java/juhe/jiangdajiuye/net/httpUtils/HttpHelper.java
@@ -7,8 +7,8 @@ import juhe.jiangdajiuye.view.activity.xuanJiang.entity.MesItemHolder;
 
 /**
  * class description here
- *
- *  提供给客户端调用的类
+ * <p>
+ * 提供给客户端调用的类
  *
  * @author wangqiang
  * @since 2018-02-14
@@ -16,25 +16,32 @@ import juhe.jiangdajiuye.view.activity.xuanJiang.entity.MesItemHolder;
 
 public class HttpHelper<T> {
     private TaskExecuteManager taskExecuteManager = TaskExecuteManager.getInstance();
-    private static HttpHelper instance ;
-    public static HttpHelper getInstance(){
-        if(null == instance){
+    private static HttpHelper instance;
+
+    public static HttpHelper getInstance() {
+        if (null == instance) {
             instance = new HttpHelper();
             instance.init();
         }
-        return instance ;
+        return instance;
+    }
+
+    public <T> T getData(T t,HttpHelper<? super T> d) {
+        return t;
     }
 
     /**
      * 在使用之前必须初始化，最好在application处
      */
-    public void init(){
+    public void init() {
         ThreadHelper.getInstance().init();
     }
+
     public void get(String url, T requestBean, IDataListener<T> iDataListener, HttpTask.Type type) {
         HttpTask httpTask = new HttpTask(url, requestBean, iDataListener, type);
         taskExecuteManager.execute(httpTask);
     }
+
     public void get(String url, MesItemHolder holder, IDataListener<T> iDataListener, HttpTask.Type type) {
         HttpTask httpTask = new HttpTask(url, holder, iDataListener, type);
         taskExecuteManager.execute(httpTask);
